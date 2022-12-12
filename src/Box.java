c32122c2eb641c7292f60f8b3bb3ac827e1d72b0
SarievIlyaz <ilyaz281290@gmail.com>
2022-12-12 14:51:14 +0300

    Initial commit

---
+++ src/Box.java
@@ -0,0 +1,10 @@
+public class Box <T> {
+private T element;
+
+    public Box(T element) {
+        this.element = element;
+    }
+    public void box (){
+        System.out.println(element);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Box <String> stringBox = new Box<>("Java-8");
+        stringBox.box();
+
+    }
+}
